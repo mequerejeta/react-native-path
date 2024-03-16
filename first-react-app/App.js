49a9e11c5393f170a1ac0de0de72bd7ed263da30
Matias <matiasezequielquerejeta@gmail.com>
2024-03-16 16:49:24 -0300

    add flat list and how to use that

--- first-react-app/App.js
+++ first-react-app/App.js
@@ -1,22 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import React , {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   const [textItem, setTextItem] = useState('')
   const [itemList, setItemList] = useState([])
-
+  const onHandlerChangeItem = (t) => setTextItem(t)
+const addItem = () => {
+  setItemList(currentItems => [
+  ...currentItems, {id: Math.random().toString(), value: textItem}
+  ])
+}
   return (
     <View style={styles.screen}>
       <View style={styles.container}>
           <TextInput placeholder='Item de lista'
          value={textItem}
-          style={styles.inputView}/>
-          <Button title='ADD'/>
+          style={styles.inputView}
+          onChangeText= {onHandlerChangeItem}/>
+          <Button title='ADD' onPress={addItem}/>
       </View>
       <View style={styles.container}>
-      <TextInput placeholder='Items' style={styles.inputView}/>
-      <Button title='Agregar'/>
+      <TextInput placeholder='Items' 
+      style={styles.inputView}
+      onChangeText= {onHandlerChangeItem}/>
+      <Button title='Agregar' onPress={addItem}/>
         {itemList.map(item => <View style={styles.itemList}><Text>{item.value}</Text></View>)}
       </View>
 
