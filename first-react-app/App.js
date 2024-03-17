14ebcf14a656317aac3676f56328a7e50bce1812
Matias <matiasezequielquerejeta@gmail.com>
2024-03-17 19:03:28 -0300

    Usus modal manage

--- first-react-app/App.js
+++ first-react-app/App.js
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, Modal, StyleSheet, Text, TextInput, View } from 'react-native';
 import React , {useState} from 'react';
 
 import { StatusBar } from 'expo-status-bar';
@@ -6,29 +6,52 @@ import { StatusBar } from 'expo-status-bar';
 export default function App() {
   const [textItem, setTextItem] = useState('')
   const [itemList, setItemList] = useState([])
+  const [itemSelected, setItemSelected] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
   const onHandlerChangeItem = (t) => setTextItem(t)
 const addItem = () => {
   setItemList(currentItems => [
   ...currentItems, {id: Math.random().toString(), value: textItem}
   ])
+  const onHandlerDelete = id => {
+    setItemList(currentItems => currentItems.filter(item => item.id !== id))
+    setItemSelected({})
+    setModalVisible(!modalVisible)
+  }
+  const onHandlerModal = id => {
+    setItemSelected(itemList.filter(item => item.id === id)[0])
+    setModalVisible(!modalVisible)
+  }
 }
   return (
     <View style={styles.screen}>
-      <View style={styles.container}>
+        <View style={styles.container}>
           <TextInput placeholder='Item de lista'
-         value={textItem}
+            value={textItem}
+           style={styles.inputView}
+            onChangeText= {onHandlerChangeItem}/>
+           <Button title='ADD' onPress={addItem}/>
+        </View>
+        <View style={styles.container}>
+          <TextInput placeholder='Items' 
           style={styles.inputView}
           onChangeText= {onHandlerChangeItem}/>
-          <Button title='ADD' onPress={addItem}/>
-      </View>
-      <View style={styles.container}>
-      <TextInput placeholder='Items' 
-      style={styles.inputView}
-      onChangeText= {onHandlerChangeItem}/>
-      <Button title='Agregar' onPress={addItem}/>
-        {itemList.map(item => <View style={styles.itemList}><Text>{item.value}</Text></View>)}
-      </View>
-
+          <Button title='Agregar' onPress={addItem}/>
+          {itemList.map(item => <View style={styles.itemList}><Text>{item.value}</Text></View>)}
+        </View>
+        <View style={styles.container}>
+           <FlatList
+           data={itemList}
+           renderItem={data => (
+            <TouchableOpacity onPress={onHandlerDelete.bind(this, data.item, id)}>
+              <View style={styles.itemList}>
+                <Text>{data.item.value}</Text>
+              </View>
+            </TouchableOpacity>
+           )}
+           keyExtractor={(item) => item.id}
+           />
+        </View>
     </View>
   );
 }
