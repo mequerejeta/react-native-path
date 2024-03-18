dbd1ba32f28cb69967cd87f158772ecc45ac8f60
Matias Ezequiel Querejeta <84789061+mequerejeta@users.noreply.github.com>
2024-03-18 17:00:35 -0300

    add split component Modal and put export, later need to put the import

--- first-react-app/App.js
+++ first-react-app/App.js
@@ -52,22 +52,7 @@ const addItem = () => {
            keyExtractor={(item) => item.id}
            />
         </View>
-        <Modal animationType= "slide"
-        visible={modalVisible}
-        >
-          <View style={styles.modalTitle}>
-              <Text>Mi Modal</Text>
-          </View>
-          <View style={styles.modalMessage}>
-            <Text>Esta seguro que desea borrar?</Text>
-          </View>
-          <View style={styles.modalMessage}>
-            <Text style={styles.modalItem}>{itemSelected.value}</Text>
-          </View>
-          <View style={styles.modalButton}>
-            <Button onPress={onHandlerDelete.bind(this, itemSelected.id)} title="CONFIRM"/>
-            </View>
-        </Modal>
+
     </View>
   );
 }
@@ -86,21 +71,5 @@ const styles = StyleSheet.create({
     borderBottomColor: 'black', 
     borderBottomWidth:1
   },
-  modalTitle: {
-    backgroundColor: '#ccc',
-    color: 'white',
-    fontSize: 18
-  },
-  modalMessage: {
-    marginBottom: 10,
-    marginBottom: 10,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  modalButton: {
-    marginTop: 15
-  },
-  modalItem: {
-    fontSize: 30
-  }
+ 
 });
